eadc8b833a1a0f083747853b8f0838d6a66b09a8
lukasb27 <lukasball@live.co.uk>
2025-12-04 20:48:05 +0000

    feat: add new CORS rules

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ app.include_router(year.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://fredball.co.uk", "http://localhost:8080", "http://localhost"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
